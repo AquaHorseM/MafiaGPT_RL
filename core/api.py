3f2a009517da2df595a79f8377b69893c8042a85
Qinwei Ma <mqw21@mails.tsinghua.edu.cn>
2024-07-17 20:54:49 +0800

    undone_initial

--- core/api.py
+++ core/api.py
@@ -1,8 +1,9 @@
 # CONFIG YOUR API HERE :
 RATE_LIMIT = 20  # sleeping time for openai per minute limitation
-TOKEN_LIMIT = 5000  # token limit per message
+TOKEN_LIMIT = 100  # token limit per message
 TEMPERATURE = 1
-MODEL = "gpt-3.5-turbo"
+# MODEL = "gpt-4-turbo"
+MODEL = "gpt-3.5-turbo-0125" #for debugging, use the cheaper api
 
 import openai, os, time
 
@@ -10,12 +11,24 @@ import openai, os, time
 DEBUG = False
 
 
-# getting open ai api key from the evnironmental variables
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# getting open ai api key from the environmental variables
+client = openai.OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
 
 # make content in openai wanted format
 def create_message(role, content):
     return {"role": role, "content": content}
+
+def get_context(messages):
+    if isinstance(messages, list) and isinstance(messages[0], dict):
+        return messages
+    context = []
+    if isinstance(messages, tuple):
+        messages = [messages]
+    elif isinstance(messages, str):
+        messages = [("user", messages)]
+    for role, content in messages:
+        context.append(create_message(role, content))
+    return context
 
 
 def send_message(
@@ -24,17 +37,19 @@ def send_message(
     """
     Sending the request to api
     """
+    messages = [
+        ("system", intro),
+        ("user", game_report[-(token_limit - len(intro) - 70) :]),
+        ("system", command),
+    ]
+    context = get_context(messages)
 
     time.sleep(time_limit_rate)
-    context = [
-        create_message("system", intro),
-        create_message("user", game_report[-(token_limit - len(intro) - 70) :]),
-        create_message("system", command),
-    ]
 
     # connecting to Openai
-    response = openai.ChatCompletion.create(
-        model=MODEL, messages=context, temperature=TEMPERATURE
+    response = openai.chat.completions.create(
+        model=MODEL, messages=context, temperature=TEMPERATURE,
+        max_tokens=token_limit, top_p=1
     )
     # just for debugging in terminal
     if DEBUG:
@@ -52,4 +67,26 @@ def send_message(
         """
         )
     # returning the response as a string
-    return response.choices[0].message["content"]
+    return response.choices[0].message.content
+
+def send_message_new(messages, agent_config = {}):
+    '''
+    A flexible function to send messages to openai
+    Messages should be a tuple or list of tuples
+    Each tuple should have two elements: role and content
+    '''
+    token_limit = agent_config.get("token_limit", TOKEN_LIMIT)
+    rate_limit = agent_config.get("rate_limit", RATE_LIMIT)
+    model_name = agent_config.get("model", MODEL)
+    temperature = agent_config.get("temperature", TEMPERATURE)
+    context = get_context(messages)
+    
+    time.sleep(rate_limit)
+        
+    # connecting to Openai
+    response = openai.chat.completions.create(
+        model=model_name, messages=context, temperature=temperature,
+        max_tokens=token_limit, top_p=1
+    )
+    # returning the response as a string
+    return response.choices[0].message.content
--- core/game.py
+++ core/game.py
@@ -2,127 +2,7 @@ import random, logging
 from core.api import send_message
 import json, re
 from prompts import render_prompts as render
-
-
-class Player:
-    def __init__(self, id):
-        self.notes = ""
-        self.is_alive = True
-        self.id = id
-        self.votes = []
-        self.special_actions_log = []
-
-    def __str__(self):
-        return f"Player {self.id}"
-
-    def targeting(self, Game, command):
-        res = send_message(
-            render.game_intro(self),
-            render.game_report(Game, self),
-            command
-            + "REMINDER: your message must include the number of player that you want to perform action on it.",
-        )
-        nums_in_res = re.findall(r"\d+", res)
-        if nums_in_res == []:
-            Game.logger.warning("No player provided in targetting.")
-            return (None, res)
-        target = int(nums_in_res[0])
-        if target not in Game.alive_players:
-            Game.logger.warning("Targeting a wrong player")
-            return (None, res)
-        return (target, res)
-
-    def vote(self, Game):
-        target, reason = self.targeting(
-            Game,
-            "Command: just send the number of the player that you want to vote for. You must not vote to yourself. if you don't want to vote anyone just send an empty response.",
-        )
-        self.votes.append(target)
-        return (target, reason)
-
-
-class Villager(Player):
-    def __init__(self, role, **kwargs):
-        super().__init__(**kwargs)
-        self.role = role
-        self.type = "villager"
-
-
-class Werewolf(Player):
-    def __init__(self, role, **kwargs):
-        super().__init__(**kwargs)
-        self.role = role
-        self.type = "werewolf"
-        self.rank = "normal"  # it may change to leader
-
-    def killing(self, Game):
-        target, reason = self.targeting(
-            Game,
-            f"Command : JUST send the number of player who you want to kill for tonight. also consider this advices : {Game.werewolves_talks}",
-        )
-        if target:
-            Game.log_submit(
-                {"event": "targeted", "content": {"player": target, "reason": reason}}
-            )
-            player = Game.get_player(target)
-            if Game.healed_guy is not player:
-                Game.kill(player)
-            self.special_actions_log.append(f"you attemped to kill player{target}")
-
-    def advicing(self, Game):
-        target, reason = self.targeting(
-            Game,
-            "Command : send a short advice on which player do you suppose for eliminating at tonight from villagers.",
-        )
-        self.log_submit(
-            {"event": "speech", "content": {"player": self.id, "context": reason}}
-        )
-        self.werewolves_talks.append(reason)
-
-
-class Medic(Villager):
-    def __init__(self, role, **kwargs):
-        super().__init__(role, **kwargs)
-        self.type = "medic"
-
-    def healing(self, Game):
-        target, reason = self.targeting(
-            Game,
-            "Command : send the number of player who you want to heal for tonight.",
-        )
-        if target:
-            Game.healed_guy = Game.get_player(target)
-            self.special_actions_log.append(f"You have healed Player number {target}")
-            Game.log_submit(
-                {"event": "healed", "content": {"player": target, "reason": reason}}
-            )
-
-
-class Seer(Villager):
-    def __init__(self, role, **kwargs):
-        super().__init__(role, **kwargs)
-        self.type = "seer"
-
-    def inquiry(self, Game):
-        target, reason = self.targeting(
-            Game,
-            "Command : send the number of player who you want to know that is werewolf or not for tonight.",
-        )
-        if target:
-            targeted_player = Game.get_player(target)
-            is_werewolf = targeted_player.type == "werewolf"
-            tag = "" if is_werewolf else "not"
-            self.special_actions_log.append(f" Player {target} is {tag} werewolf")
-            Game.log_submit(
-                {
-                    "event": "inquiried",
-                    "content": {
-                        "player": target,
-                        "context": is_werewolf,
-                        "reason": reason,
-                    },
-                }
-            )
+from core.baseline_players import Villager, Werewolf, Medic, Seer
 
 
 class Game:
@@ -179,6 +59,8 @@ class Game:
         self.all_players = self.alive_players
 
     def get_player(self, id):
+        #debug 
+        print(id, self.all_players[id])
         return self.all_players[id]
 
     def get_alive_werewolves(self):
@@ -233,11 +115,7 @@ class Game:
     def run_day(self):
         self.report = []
         for Player in self.alive_players:
-            res = send_message(
-                render.game_intro(Player),
-                render.game_report(self, Player),
-                render.speech_command(),
-            ).replace("\n", " ")
+            res = Player.speak(self, render.speech_command()).replace("\n", " ") #
             self.report.append(str(Player) + "opinion : " + res)
             self.log_submit(
                 {"event": "speech", "content": {"player": Player.id, "context": res}}
