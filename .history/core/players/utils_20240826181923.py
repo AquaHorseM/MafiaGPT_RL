e2a5480c00dc08fd06a53a5f147f92afdbf568e0
Qinwei Ma <mqw21@mails.tsinghua.edu.cn>
2024-08-27 16:38:16 +0800

    reflex written (not debugged yet)

---
+++ .history/core/players/utils_20240826181923.py
@@ -0,0 +1,38 @@
+import os
+import re, pickle
+
+def get_prompt(prompt_path, replacements):
+    if not prompt_path.endswith(".txt"):
+        prompt_path = prompt_path + ".txt"
+    with open(prompt_path, 'r') as file:
+        content = file.read()
+        #allow user to comment using ''', '''
+        content = content.split("'''")
+        content = [content[i] if i % 2 == 0 else "" for i in range(len(content))]
+        content = "".join(content)
+
+        for key, value in replacements.items():
+            content = content.replace(key, value)
+    return content
+
+def get_target_from_response(response):
+    #find the first number in the response
+    try:
+        target = int(re.search(r"\d+", response).group())
+    except:
+        target = None
+    return target
+
+def parse_data(data):
+    #data should be a list in the form of [hidden_state, tuple_of_events * n, hidden_state, tuple_of_events * n, ...]
+    #return a list of tuples in the form of [(hidden_state, tuple_of_events (merged), new_hidden_state), ...]
+    #hidden state should be np tensor
+    res = []
+    prev_hstate_index = 0
+    pair = [data[0]]
+    for i in range(1, len(data)):
+        if isinstance(data[i], tuple):
+            res.append((data[prev_hstate_index], data[prev_hstate_index+1:i], data[i]))
+            prev_hstate_index = i
+        else: #if it is a hidden state
+            pair.append(data[i])
