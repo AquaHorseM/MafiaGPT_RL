9627397739338ceeacfdaa324959144076ae5d2f
Qinwei Ma <mqw21@mails.tsinghua.edu.cn>
2024-09-27 11:10:24 +0800

    load & showdata modified

--- load_data.py
+++ load_data.py
@@ -1,7 +1,9 @@
 import pickle
 from core.event import Event
+from core.data import DataTree
 # Load data from file
+
 data_path = "data/game_0_data.pkl"
 with open(data_path, "rb") as f:
-    data = pickle.load(f)
-data.show_info()
+    data: DataTree = pickle.load(f)
+data.show_info(interactive = True)
