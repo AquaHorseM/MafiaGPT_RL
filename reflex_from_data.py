28297c9a5d0f7c91efaa02f15b9cb991729ed524
Qinwei Ma <mqw21@mails.tsinghua.edu.cn>
2024-09-27 16:49:45 +0800

    debug

--- reflex_from_data.py
+++ reflex_from_data.py
@@ -7,18 +7,15 @@ from core.api import load_client
 from argparse import ArgumentParser
     
 parser = ArgumentParser()
-parser.add_argument("--num_processes", type=int, default=1)
 parser.add_argument("--openai_config_path", type=str, default="openai_config.yaml")
 parser.add_argument("--config_path", type=str, default="configs/player_configs_v01.json")
 parser.add_argument("--start_idx", type=int, default=0)
 parser.add_argument("--ckpt_path", type=str, default=None)
-parser.add_argument("--reflex-only",default=False, action="store_true")
 parser.add_argument("--data-path", type=str, default="data/game_1_data.pkl")
-parser.add_argument("--train",default=False, action="store_true")
 
 def reflex_from_path(ipt, client, path):
-    idx, player_configs, train = ipt
-    game = Game(999, train, client, path)
+    idx, player_configs = ipt
+    game = Game(999, True, client, path)
     game.set_players(player_configs)
     game.load_data(path)
     game.all_players_reflex()
@@ -30,11 +27,4 @@ if __name__ == "__main__":
     run_game = partial(reflex_from_path, client=client, path = args.data_path)
     with open(args.config_path, "r") as f:
         player_configs = json.load(f)["players"]
-    if args.num_processes == 1:
-        for i in range(args.num_games):
-            run_game((args.start_idx, player_configs, args.train))
-    else:
-        ipt = [(args.start_idx + i, player_configs, args.train) for i in range(args.num_games)]
-        if __name__ == "__main__":
-            with multiprocessing.Pool(args.num_processes) as pool:
-                pool.map(run_game, ipt)
+    run_game((args.start_idx, player_configs))
