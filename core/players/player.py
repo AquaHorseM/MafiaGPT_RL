77b7a856475242e6ca0614a02425c257dc1cdc97
Qinwei Ma <mqw21@mails.tsinghua.edu.cn>
2024-10-05 17:50:35 +0800

    debug

--- core/players/player.py
+++ core/players/player.py
@@ -226,8 +226,8 @@ class Player:
     
     
     def _get_proposals_from_response_SpeakThreeStep(self, response):
-        first_pattern = r"First Proposal:(.*?)\n"
-        second_pattern = r"Second Proposal:(.*?)\n"
+        first_pattern = r"First Proposal:(.*)\n"
+        second_pattern = r"Second Proposal:(.*)\n"
         first_match = re.search(first_pattern, response)
         second_match = re.search(second_pattern, response)
         
@@ -248,12 +248,10 @@ class Player:
         return first_answer
     
     def _get_final_choice_from_response_SpokeThreeStep(self, response):
-        print(f"DEBUG: response is {response}")
-        first_pattern = r".*My final speech is: (.*?)"
+        first_pattern = r".*My final speech is: (.*)"
         first_match = re.search(first_pattern, response)
         
         first_answer = first_match.group(1) if first_match else None
-        print(f"DEBUG: first answer is {first_answer}")
         return first_answer
     
     def _speak_multiagent(self):
