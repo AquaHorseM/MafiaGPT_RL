38fea569e087d9a9d2b8fa8aa78171726b4c796a
Qinwei Ma <mqw21@mails.tsinghua.edu.cn>
2024-10-08 23:19:59 +0800

    debug

--- core/game_env.py
+++ core/game_env.py
@@ -691,13 +691,13 @@ class WerewolfGameEnv:
                 player_id = draft["player_id"]
                 speak_action = self.all_players[player_id]._speak_with_other_proposal(draft)
                 actions[player_id] = speak_action
-                self.logger.debug(f"speak action: {speak_action}")
+                self.logger.debug(f"player: {player_id}'s speak action: {speak_action}")
             elif draft["cur_action"] == "vote":
                 self.logger.debug("Vote other proposal!")
                 player_id = draft["player_id"]
                 vote_action = self.all_players[player_id]._vote_with_other_proposal(draft)
                 actions[player_id] = vote_action
-                self.logger.debug(f"vote action: {vote_action}")
+                self.logger.debug(f"player: {player_id}'s vote action: {vote_action}")
         obs, state, rewards, dones, info, avail_actions = self.step(actions)
         self.logger.debug("Stepped!")
         if self.postprocess_step(actions, dones, info):
