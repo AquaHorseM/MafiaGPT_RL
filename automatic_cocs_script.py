db34538178b452e0706d40849060cf0924482a93
ShiJZ <100902397+JingzheShi@users.noreply.github.com>
2024-10-16 18:04:09 +0800

    s

--- automatic_cocs_script.py
+++ automatic_cocs_script.py
@@ -12,7 +12,6 @@ import itertools
 from collections import defaultdict
 
 def generate_version_tuples():
-<<<<<<< HEAD
     # All possible 4-element combinations of numbers 0-5 without repetition
     all_tuples = list(itertools.permutations(range(6), 4))
     
@@ -22,24 +21,10 @@ def generate_version_tuples():
     
     # Hash to index mapping to ensure reproducibility
     hash_to_index = {hash(tup): tup for tup in all_tuples}
-=======
-    count = 0
-    while True:
-        # All possible 4-element combinations of numbers 0-7 without repetition
-        all_tuples = list(itertools.permutations(range(8), 4))
-        
-        # We need exactly 40 unique tuples
-        selected_tuples = []
-        count_by_position = [defaultdict(int) for _ in range(4)]
-        
-        # Hash to index mapping to ensure reproducibility
-        hash_to_index = {hash(tup+tuple([count])): tup for tup in all_tuples}
->>>>>>> origin/master
 
         # Sorting hashes to keep the selection order consistent
         sorted_hashes = sorted(hash_to_index.keys())
 
-<<<<<<< HEAD
     for h in sorted_hashes:
         tuple_candidate = hash_to_index[h]
         # Check if this tuple can be used without exceeding the position count limits
@@ -51,13 +36,6 @@ def generate_version_tuples():
                     break
             if valid:
                 selected_tuples.append(tuple_candidate)
-=======
-        for h in sorted_hashes:
-            tuple_candidate = hash_to_index[h]
-            # Check if this tuple can be used without exceeding the position count limits
-            if len(selected_tuples) < 40:
-                valid = True
->>>>>>> origin/master
                 for i in range(4):
                     if count_by_position[i][tuple_candidate[i]] >= 5:
                         valid = False
@@ -78,17 +56,10 @@ precomputed_tuples = generate_version_tuples()
 print(precomputed_tuples)
 def idx_to_version_tuple(idx):
     # Return the tuple corresponding to the given index
-<<<<<<< HEAD
     if 0 <= idx < 30:
         return precomputed_tuples[idx]
     else:
         raise ValueError("Index out of allowed range (0-29).")
-=======
-    if 0 <= idx < 40:
-        return precomputed_tuples[idx]
-    else:
-        raise ValueError("Index out of allowed range (0-39).")
->>>>>>> origin/master
 
 
 
@@ -99,7 +70,6 @@ def idx_to_version_tuple(idx):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-<<<<<<< HEAD
     parser.add_argument('--war_folder', type=str, default = './cocs_ablation_1')
     parser.add_argument('--num_games_per_battle', type=int, default = 1)
     parser.add_argument('--num_process_per_battle', type=int, default = 1)
@@ -125,30 +95,12 @@ if __name__ == "__main__":
     assert len(args.clans_tags) == 6
     
     assert args.num_battles_in_total == 30
-=======
-    parser.add_argument('--war_folder', type=str, default = './clash_of_clanS_shijz_15')
-    parser.add_argument('--clans_notes_folder', type = str, default = './shijz_test_15/notes')
-    parser.add_argument('--clans_tags', type=str, default='')
-    parser.add_argument('--num_games_per_battle', type=int, default = 1)
-    parser.add_argument('--num_process_per_battle', type=int, default = 1)
-    parser.add_argument('--num_battle_parallel', type=int, default = 5)
-    parser.add_argument('--num_battles_in_total', type=int, default = 40)
-    parser.add_argument('--start_from', type=int, default=8)
-    
-    args = parser.parse_args()
-    if args.clans_tags == '':
-        args.clans_tags = ['notes_v{}'.format(i) for i in range(8)]
-    assert len(args.clans_tags) == 8
-    
-    assert args.num_battles_in_total == 40
->>>>>>> origin/master
     
     args_list = []
     
     idx_to_version_list = list()
     
     for idx in range(args.num_battles_in_total):
-<<<<<<< HEAD
         version_tuple = idx_to_version_tuple(idx)
         idx_to_version_list.append(version_tuple)
         notes_folders = [args.notes_dir_list[v] for v in version_tuple]
@@ -157,14 +109,6 @@ if __name__ == "__main__":
         args_list.append(('battle_'+str(idx), args.war_folder, *notes_folders, *tags, args.num_games_per_battle, args.num_process_per_battle,
                             *player_types,
         ))
-=======
-        if idx >= args.start_from:
-            version_tuple = idx_to_version_tuple(idx)
-            idx_to_version_list.append(version_tuple)
-            notes_folders = [os.path.join(args.clans_notes_folder, 'notes_v'+str(v)) for v in version_tuple]
-            tags = [args.clans_tags[v] for v in version_tuple]
-            args_list.append(('battle_'+str(idx), args.war_folder, *notes_folders, *tags, args.num_games_per_battle, args.num_process_per_battle))
->>>>>>> origin/master
     
     json.dump(idx_to_version_list, open(os.path.join(args.war_folder, 'idx_to_version_list.json'), 'w'), indent=4)
     # json.dump(idx_to_version_list, open('idx_to_version_list.json', 'w'), indent=4)
